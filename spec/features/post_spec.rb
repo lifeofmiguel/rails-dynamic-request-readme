56f8fa7341970315b4842a07269c133021f7ac67
Miguel Angel Morales <mmorales21500@live.com>
2021-04-11 10:33:59 -0400

    commit tree 5b29237d3cdb24e48140521f2c98db938102b9e5

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -11,4 +11,9 @@ describe 'navigate' do
     visit "/posts/#{@post.id}"
     expect(page.status_code).to eq(200)
   end
+
+  it 'shows the description on the show page in a p tag' do
+    visit "/posts/#{@post.id}"
+    expect(page).to have_css("p", text: "My post desc")
+  end 
 end 
